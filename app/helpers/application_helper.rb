e9643bc00fe1c85ae3de01a36e32026bcc068d74
mokoaki <mokoriso@gmail.com>
2014-04-13 15:52:10 +0900

    refa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,2 @@
 module ApplicationHelper
-  def youbi(wday)
-    (@youbi ||= ['日','月','火','水','木','金','土'])[wday]
-  end
-
-  def wrap(description)
-    max_width        = 30
-    zero_width_space = '&#8203;'
-    regex            = /.{1,#{max_width}}/
-
-    description.split(/\r\n|\r|\n/).map do |description_line|
-      description_line.split(' ').map do |text|
-        text.scan(regex).map do |split_text|
-          h split_text
-        end.join(zero_width_space)
-      end.join(' ')
-    end.join('<br />')
-  end
 end
